7bddee27b98479311d90dec9f0d38ba667abe423
unknown <you@example.comrenzoromero350@gmail.com>
2026-07-01 18:01:36 -0700

    Lesson 01

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,26 @@
 
 export default function StudentWork() {
   //add variables here
+  const name = 'Renzo Romero';
+  const age = '28';
+  const hobbies = [
+    'Working Out',
+    'Video Games',
+    '3D Art',
+    'Card Games',
+    'Web Development',
+  ];
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h2>Game Developer looking to learn more about Software Development</h2>
+      <p>
+        Hi, my name is Renzo Romero, I live in the Bay Area and enjoy exercise
+      </p>
+      <ul>
+        {hobbies.map((hobby) => {
+          return <li>{hobby}</li>;
+        })}
+      </ul>
     </div>
   );
 }
